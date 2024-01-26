dafaf8f8e59c7947c86ca89c9a1dc2d0522fc01d
kdiffin <vhiunese@gmail.com>
2024-01-26 22:46:32 +0400

    changed next conf

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
