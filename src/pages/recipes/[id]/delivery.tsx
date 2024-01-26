e9fa334c080c77cdb81cb6596aaaacc1d33e1d44
kdiffin <vhiunese@gmail.com>
2024-01-27 00:00:08 +0400

    added all

--- src/pages/recipes/[id]/delivery.tsx
+++ src/pages/recipes/[id]/delivery.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Check } from "lucide-react";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Map from "~/components/recipes/Map";
+import { TypographyH1 } from "~/components/typography/TypographyH1";
+import { TypographyH2 } from "~/components/typography/TypographyH2";
 const location2 = {
   lat: 37.42216,
   lng: -122.08427,
-}; // our location object from earlier
-
+};
 function Delivery() {
   const [location, setLocation] = useState();
+
+  console.log(location);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -20,10 +26,19 @@ function Delivery() {
 
   return (
     <div>
-      {" "}
-      {location && location.latitude ? (
+      <TypographyH2>Order taken successfully!!</TypographyH2>{" "}
+      <div className="py-2"></div>
+      {location ? (
         <Map location={location} zoomLevel={17} />
-      ) : null}
+      ) : (
+        <div className="h-[70vh] bg-muted"> </div>
+      )}
+      <div className="py-2"></div>
+      <Link href={"/"}>
+        <Button className="mt-4">
+          Done <Check />
+        </Button>
+      </Link>
     </div>
   );
 }
