ba22b01eaee0efbfb1f4e3ac6cf9a95cb9a1db40
kdiffin <vhiunese@gmail.com>
2024-01-26 23:49:05 +0400

    added all

--- src/pages/recipes/[id]/delivery.tsx
+++ src/pages/recipes/[id]/delivery.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Map from "~/components/recipes/Map";
+const location2 = {
+  lat: 37.42216,
+  lng: -122.08427,
+}; // our location object from earlier
 
 function Delivery() {
-  return <div>a</div>;
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
+        setLocation({ lat: latitude, lng: longitude });
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      {location && location.latitude ? (
+        <Map location={location} zoomLevel={17} />
+      ) : null}
+    </div>
+  );
 }
 
 export default Delivery;
